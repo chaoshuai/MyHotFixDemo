faa86df4eab56d3e83d06a7d58989317c2eb68d4
chaoshuai <15210629767@163.com>
2017-10-19 12:37:06 +0800

    Initial commit

---
+++ app/src/main/java/cn/cibn/myhotfixdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.cibn.myhotfixdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getDir("dex", 0);
+    }
+}
